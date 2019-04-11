f9ff2478f627fcd9aeda2238b54b96c95833cba9
dilip.duraiswamy <dilip.duraiswamy@roi.actcorp.in>
2019-04-11 22:22:15 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, Output, EventEmitter} from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import {WebcamImage, WebcamInitError, WebcamUtil} from 'ngx-webcam';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,49 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cameraApp';
+  url='';
+
+ 
+
+
+  // toggle webcam on/off
+  public showWebcam = true;
+  
+  public errors: WebcamInitError[] = [];
+
+  // webcam snapshot trigger
+  private trigger: Subject<void> = new Subject<void>();
+  
+  public ngOnInit(): void {
+    WebcamUtil.getAvailableVideoInputs()
+      .then((mediaDevices: MediaDeviceInfo[]) => {
+        
+      });
+  }
+
+  public triggerSnapshot(): void {
+    this.trigger.next();
+  }
+
+  
+
+  public handleInitError(error: WebcamInitError): void {
+    this.errors.push(error);
+  }
+
+  
+
+  public handleImage(webcamImage: WebcamImage): void {
+    console.info('received webcam image', webcamImage);
+    this.url=webcamImage.imageAsDataUrl;
+
+  }
+
+ 
+
+  public get triggerObservable(): Observable<void> {
+    return this.trigger.asObservable();
+  }
+
+
 }
